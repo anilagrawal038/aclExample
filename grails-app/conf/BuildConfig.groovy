875a04315b0b1551a87ee87733a612c642c87df3
anil <anil.agrawal@intelligrape.com>
2015-07-19 19:49:58 +0530

    change in build config

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -11,8 +11,9 @@ grails.project.fork = [
         // configure settings for compilation JVM, note that if you alter the Groovy version forked compilation is required
         //  compile: [maxMemory: 256, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
 
+
         // configure settings for the test-app JVM, uses the daemon by default
-        test   : [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, daemon: true],
+        test   : false,
         // configure settings for the run-app JVM
         run    : false,
         //[maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
